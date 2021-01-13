f4697fe1acdfd4b607b1e88ad254a413dd890b04
NSEIT <NSEIT@WEST00042>
2021-01-13 19:34:34 +0530

    security harcoded credentials

---
+++ SecureApp/src/main/java/com/nirajs/secureapp/AppSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.nirajs.secureapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+	@Bean//need object of UserDetailsService and want to a bean
+	@Override
+	public UserDetailsService userDetailsServiceBean() throws Exception {
+		List<UserDetails> users=new ArrayList<>();
+		users.add(User.withDefaultPasswordEncoder().username("niraj").password("1234").roles("user").build());
+		users.add(User.withDefaultPasswordEncoder().username("saatvik").password("1234").roles("admin").build());
+		
+		return new InMemoryUserDetailsManager(users);
+	}
+	
+	
+	
+
+}
---
+++ SecureApp/src/main/java/com/nirajs/secureapp/HomeController.java
@@ -0,0 +1,16 @@
+package com.nirajs.secureapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String home()
+	{
+		System.out.println("home page requeste");
+		
+		return "home";
+	}
+
+}
